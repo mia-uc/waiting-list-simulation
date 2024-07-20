6111e00f13e2c7d57de42442bcef8507cc30cc71
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2024-07-19 23:15:15 -0400

    feat: :sparkles: queue cmp

--- pipeline.sh
+++ pipeline.sh
@@ -1,22 +1,22 @@
-echo "clients"
-python run.py clients
-python kpis.py clients
-rm -r clients
+# echo "clients"
+# python run.py clients
+# python kpis.py clients
+# rm -r clients
 
-echo "clients_plus_cs"
-python run.py clients_plus_cs
-python kpis.py clients_plus_cs
-rm -r clients_plus_cs
+# echo "clients_plus_cs"
+# python run.py clients_plus_cs
+# python kpis.py clients_plus_cs
+# rm -r clients_plus_cs
 
-echo "clients_plus_seller"
-python run.py clients_plus_seller
-python kpis.py clients_plus_seller
-rm -r clients_plus_seller
+# echo "clients_plus_seller"
+# python run.py clients_plus_seller
+# python kpis.py clients_plus_seller
+# rm -r clients_plus_seller
 
-echo "clients_seller_and_cs"
-python run.py clients_seller_and_cs
-python kpis.py clients_seller_and_cs
-rm -r clients_seller_and_cs
+# echo "clients_seller_and_cs"
+# python run.py clients_seller_and_cs
+# python kpis.py clients_seller_and_cs
+# rm -r clients_seller_and_cs
 
 echo "clients_q_1"
 python run.py clients_q_1
@@ -28,7 +28,7 @@ python run.py clients_q_0
 python kpis.py clients_q_0
 rm -r clients_q_0
 
-echo "clients_priority_req"
-python run.py clients_priority_req
-python kpis.py clients_priority_req
-rm -r clients_priority_req
+# echo "clients_priority_req"
+# python run.py clients_priority_req
+# python kpis.py clients_priority_req
+# rm -r clients_priority_req
